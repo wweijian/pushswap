45bc5b597439676ad9d19974e3494e9b3866a76f
wweijian <hoe.weijian@gmail.com>
2025-05-29 11:06:13 +0800

    v3 complete

--- v3/includes/push_swap.h
+++ v3/includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: wjhoe <wjhoe@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 14:10:55 by wjhoe             #+#    #+#             */
-/*   Updated: 2025/05/28 13:16:48 by wjhoe            ###   ########.fr       */
+/*   Updated: 2025/05/28 16:37:16 by wjhoe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,15 @@ int		check_duplicates(int *stack, int count);
 // void	solve_stack(int *stack_a, int count);
 int		*find_solution(int *stack, int count);
 void	write_solution(int *stack_a, int *stack_b, int count);
+void	rotate_to_min(int *stack_a, int *stack_b, int count);
+void	rotate_to_destination(int *stack_a, int count);
 
-/* STACK SORTING ALGORITHMS */
-void	find_anchor(int *stack_a, int *stack_b, int count);
-void	rev_rotate_to_anchor_a(int *stack_a, int *stack_b, int count);
-void	rev_rotate_to_anchor_b(int *stack_a, int *stack_b, int count);
-void	rotate_to_anchor_a(int *stack_a, int *stack_b, int count);
-void	rotate_to_anchor_b(int *stack_a, int *stack_b, int count);
+// /* STACK SORTING ALGORITHMS */
+// void	find_anchor(int *stack_a, int *stack_b, int count);
+// void	rev_rotate_to_anchor_a(int *stack_a, int *stack_b, int count);
+// void	rev_rotate_to_anchor_b(int *stack_a, int *stack_b, int count);
+// void	rotate_to_anchor_a(int *stack_a, int *stack_b, int count);
+// void	rotate_to_anchor_b(int *stack_a, int *stack_b, int count);
 
 /* STACK SORTING FUNCTIONS */
 void	swap_top(int *stack_a, int *stack_b, int count);
@@ -48,11 +50,11 @@ void	write_rotate(int *stack_a, int *stack_b, int direction);
 void	reverse_move_stack(int *stack, int count);
 void	move_stack(int *stack, int count);
 
-/* SWAP UTILITIES*/
-void	swap_a(int *stack_a, int *stack_b, int count);
-void	swap_b(int *stack_a, int *stack_b, int count);
-void	check_swap_a(int *stack_a, int *stack_b, int count);
-void	check_swap_b(int *stack_a, int *stack_b, int count);
+// /* SWAP UTILITIES*/
+// void	swap_a(int *stack_a, int *stack_b, int count);
+// void	swap_b(int *stack_a, int *stack_b, int count);
+// void	check_swap_a(int *stack_a, int *stack_b, int count);
+// void	check_swap_b(int *stack_a, int *stack_b, int count);
 
 /* FINALIZING UTILITIES*/
 int		verify_stack_a(int *stack_a, int count);
--- v3/srcs/solve.c
+++ v3/srcs/solve.c
@@ -6,7 +6,7 @@
 /*   By: wjhoe <wjhoe@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 15:39:39 by wjhoe             #+#    #+#             */
-/*   Updated: 2025/05/28 15:44:57 by wjhoe            ###   ########.fr       */
+/*   Updated: 2025/05/29 11:05:19 by wjhoe            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,13 @@ static void	split_stack(int *stack_a, int *stack_b, int count, int index)
 {
 	int	i;
 	int	median;
-	int	rotation;
 	int	push_count;
 
-	rotation = 0;
 	push_count = 0;
 	median = find_median(stack_a, count);
 	if (count - index < 3)
 		return ;
-	while (rotation < count - index || push_count < median - index)
+	while (push_count < median - index)
 	{
 		i = top_of_stack(stack_a, count);
 		while (stack_a[i] <= median)
@@ -36,8 +34,8 @@ static void	split_stack(int *stack_a, int *stack_b, int count, int index)
 			i = push_a_to_b(stack_a, stack_b, count);
 			push_count++;
 		}
-		rotate_stack(stack_a, NULL, count);
-		rotation++;
+		if (push_count != median - index)
+			rotate_stack(stack_a, NULL, count);
 	}
 	i = top_of_stack(stack_a, count);
 	if (count - i > 3)
@@ -60,7 +58,7 @@ void	stack_dump(int *stack_a, int *stack_b, int count)
 	j = push_b_to_a(stack_a, stack_b, count);
 	i = top_of_stack(stack_a, count);
 	if (stack_b[j] < stack_a[i]
-		&& (stack_b[j] > stack_a[count - 1] || stack_a[count - 1] == count)) 
+		&& (stack_b[j] > stack_a[count - 1] || stack_a[count - 1] == count))
 		return (stack_dump(stack_a, stack_b, count));
 }
 
@@ -68,28 +66,30 @@ void	populate_stack_a(int *stack_a, int *stack_b, int count)
 {
 	int	i;
 	int	j;
-
 	i = top_of_stack(stack_a, count);
 	j = top_of_stack(stack_b, count);
 	if (j == count || i == 0)
 		return ;
-	while (stack_b[j] > stack_a[i])
-		rotate_stack(stack_a, NULL, count);
+	rotate_to_min(stack_a, stack_b, count);
 	stack_dump(stack_a, stack_b, count);
-	while (stack_a[count - 1] != count)
-		reverse_rotate_stack(stack_a, NULL, count);
 	return (populate_stack_a(stack_a, stack_b, count));
 }
 
 void	write_solution(int *stack_a, int *stack_b, int count)
 {
-	for (int i = 0; i < count; i++)
-		printf("index %d:\t%d\t\t%d\n", i, stack_a[i], stack_b[i]);
+	// for (int i = 0; i < count; i++)
+	// 	printf("index %d:\t%d\t\t%d\n", i, stack_a[i], stack_b[i]);
 	split_stack(stack_a, stack_b, count, 0);
+	// for (int i = 0; i < count; i++)
+	// 	printf("index %d:\t%d\t\t%d\n", i, stack_a[i], stack_b[i]);
+	// printf("\nmake base\n-------\n");
 	make_base(stack_a, count);
-	for (int i = 0; i < count; i++)
-		printf("index %d:\t%d\t\t%d\n", i, stack_a[i], stack_b[i]);
-	populate_stack_a(stack_a, stack_b, count);
+	// for (int i = 0; i < count; i++)
+	// 	printf("index %d:\t%d\t\t%d\n", i, stack_a[i], stack_b[i]);
+	// printf("\npopulate stack a\n-------\n");
+	// for (int i = 0; i < 40; i++)
+		populate_stack_a(stack_a, stack_b, count);
+	rotate_to_destination(stack_a, count);
 	for (int i = 0; i < count; i++)
 		printf("index %d:\t%d\t\t%d\n", i, stack_a[i], stack_b[i]);
 }
@@ -97,7 +97,7 @@ void	write_solution(int *stack_a, int *stack_b, int count)
 /* 
 	for (int i = 0; i < count; i++)
 		printf("index %d:\t%d\t\t%d\n", i, stack_a[i], stack_b[i]);
- */
+*/
 
 /*
 	sa
@@ -108,7 +108,7 @@ void	write_solution(int *stack_a, int *stack_b, int count)
 	pa
 */
 
-/* 
+/*  // split_stack implementation that does not binary split
 if (stack_a[i] < median)
 {
 	if (stack_a[i] < stack_a[i + 1])
@@ -120,3 +120,21 @@ if (stack_a[i] < median)
 /* 
  printf("hi %d  %d  %d  %d\n", j, stack_b[j], i, stack_a[i]);
  */
+
+/* static void	split_stack(int *stack_a, int *stack_b, int count, int index)
+{
+	int	i;
+
+	i = 0;
+	while (count - i > 3)
+	{
+		if (stack_a[i] < count - 2)
+		{
+			push_a_to_b(stack_a, stack_a, count);
+			i++;
+		}
+		else
+			rotate_stack(stack_a, NULL, count);
+	}
+	(void) index;
+} */
